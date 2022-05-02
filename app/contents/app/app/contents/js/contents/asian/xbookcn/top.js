c4405e79e6ef51e106e4ff2ffcb7687af4f5369c
Vicky <vicky@google.com>
2022-05-02 15:53:51 +0800

    Bug Fixed

---
+++ app/contents/app/app/contents/js/contents/asian/xbookcn/top.js
@@ -0,0 +1,5 @@
+document.write('<br><div align="center"><center>');
+document.write('<a href="/">首頁</a> | <a href="/class/">分類</a> | <a href="/read/">短篇</a>');
+document.write(' | <a href="javascript:window.location.href=\'https://www.xbookcn.com\'+window.location.pathname ">繁體</a>');
+document.write(' | <a href="javascript:window.location.href=\'https://www.xbookcn.net\'+window.location.pathname ">簡體</a>');
+document.write(' </center></div><br>');
