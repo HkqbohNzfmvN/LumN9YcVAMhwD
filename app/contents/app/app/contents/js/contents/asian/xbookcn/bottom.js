2255df48a5c09f2d0a3a6091f3bc611ce9800ae5
HkqbohNzfmvN <102346913+HkqbohNzfmvN@users.noreply.github.com>
2024-08-05 16:26:37 -0400

    Update bottom.js

--- app/contents/app/app/contents/js/contents/asian/xbookcn/bottom.js
+++ app/contents/app/app/contents/js/contents/asian/xbookcn/bottom.js
@@ -1,16 +1,1 @@
-document.write ('<br><br><br>');
-document.write('<div align="center"><center>');
-document.write('<a href="/">首頁</a> | <a href="/class/">分類</a> | <a href="/read/">短篇</a>  | <a href="/contact/">聯係</a>');
-document.write(' | <a href="javascript:window.location.href=\'https://www.xbookcn.com\'+window.location.pathname ">繁體</a>');
-document.write(' | <a href="javascript:window.location.href=\'https://www.xbookcn.net\'+window.location.pathname ">簡體</a>');
-document.write(' </center></div><br>');
 
-//google analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-99929-1', 'auto');
-  ga('send', 'pageview');
-//google analytics
