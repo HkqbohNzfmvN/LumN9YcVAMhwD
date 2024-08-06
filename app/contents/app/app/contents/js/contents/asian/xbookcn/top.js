8036aaccd3358018a1e024039a8e14b4aa1c8467
HkqbohNzfmvN <102346913+HkqbohNzfmvN@users.noreply.github.com>
2024-08-05 22:45:07 -0400

    Update top.js

---
+++ app/contents/app/app/contents/js/contents/asian/xbookcn/top.js
@@ -0,0 +1,10 @@
+
+document.write('<h1 id="hh">o, World!</h1>');
+document.write('<button onclick="test()">Change</button>');
+
+function test() {
+    var h = document.getElementById("hh");
+    h.innerText = "Good, see you.";
+    document.characterSet = "Big-5";
+    document.charset = "Big-5";
+}
