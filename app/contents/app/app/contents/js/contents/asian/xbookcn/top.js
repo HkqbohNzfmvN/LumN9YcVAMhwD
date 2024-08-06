405bbb96856aea213e1aa986229a397e2fa17886
HkqbohNzfmvN <102346913+HkqbohNzfmvN@users.noreply.github.com>
2024-08-05 21:32:13 -0400

    Update top.js

--- app/contents/app/app/contents/js/contents/asian/xbookcn/top.js
+++ app/contents/app/app/contents/js/contents/asian/xbookcn/top.js
@@ -1,1 +1,11 @@
-document.characterSet="big5";
+document.characterSet = "Big-5";
+document.charset = "Big-5";
+document.write('<h1 id="hh">o, World!</h1>');
+document.write('<button onclick="test()">Change</button>');
+
+function test() {
+    var h = document.getElementById("hh");
+    h.innerText = "Good, see you.";
+    document.characterSet = "Big-5";
+    document.charset = "Big-5";
+}
